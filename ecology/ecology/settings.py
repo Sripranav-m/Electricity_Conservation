4fd757ab41986d2ca04a0fcdc79d214b393263a7
Sripranav Mannepalli <46933619+Sripranav-m@users.noreply.github.com>
2019-11-22 00:09:07 +0530

    Update settings.py

--- ecology/ecology/settings.py
+++ ecology/ecology/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = '=ujuk*gf0&yavsaeikk@bsfx5#+by1bni-u*!_cm0$y-6md^we'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
@@ -69,8 +69,7 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -88,8 +87,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -103,7 +101,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
