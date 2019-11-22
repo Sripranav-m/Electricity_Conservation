2709801149b748aa6075187d6d17db0a2b9247b5
Sripranav-m <46933619+Sripranav-m@users.noreply.github.com>
2019-11-23 02:19:29 +0530

    third-commit

--- ecology/ranking/admin.py
+++ ecology/ranking/admin.py
@@ -5,5 +5,5 @@ from .models import ranking,query
 admin.site.register(ranking)
 admin.site.register(query)
 
-admin.site.site_header = "SRIPRANAV MANNEPALLI"
+admin.site.site_header = "ELECTRICITY CONSERVATION"
 admin.site.index_title = "RANKING SYSTEM"
